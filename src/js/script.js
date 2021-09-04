ee0555e4a83896e5d8a30b6daf52406ca8b10868
Max <maxrostowoj@gmail.com>
2021-09-04 14:37:17 +0300

    add functional for navigation items without scrolling

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,36 @@ import "../img/logos3.png";
 import "../img/logos4.png";
 import "../img/expirience.jpg";
 import "../img/backtic.png";
+
+
+window.addEventListener('DOMContentLoaded', () => {
+
+
+    // Tabs in navigation
+    const navigation = document.querySelector('.header__navigation'),
+          itemsParent = document.querySelector('.header__items'),
+          itemsChildren = document.querySelectorAll('.header__item'),
+          underLine = navigation.querySelector('.header__under');
+
+    underLine.style.width = `${itemsChildren[0].offsetWidth}px`;
+
+    itemsChildren.forEach(item => {
+        item.addEventListener('click', () => {
+
+            function getWidth(){
+                let widthElem = item.offsetWidth;
+                let leftElem = item.offsetLeft;
+                underLine.style.width = widthElem + 'px';
+                underLine.style.transition = '0.4s ease-in-out';
+                underLine.style.marginLeft = `${leftElem}px`;
+            }
+
+            itemsChildren.forEach( item => {
+                item.classList.remove('active__item');
+            });
+
+            item.classList.add('active__item');
+            getWidth();
+        });
+    });
+});
